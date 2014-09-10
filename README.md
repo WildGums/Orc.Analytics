37bb57a34eaa4679d3989876feede0e514e7d8f2
Geert van Horrik <geert@catenalogic.com>
2014-09-10 13:50:48 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Orc.Analytics
+
+This library makes it very easy to integration Google Analytics with Catel projects.
+
+# Introduction
+
+// TODO: Write introduction
---
+++ src/Orc.Analytics.Example/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,58 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MainWindowViewModel.cs" company="Orchestra development team">
+//   Copyright (c) 2008 - 2014 Orchestra development team. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Orc.Analytics.Example.ViewModels
+{
+    using System.Threading.Tasks;
+    using Catel.Logging;
+    using Catel.MVVM;
+
+    /// <summary>
+    /// MainWindow view model.
+    /// </summary>
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private static readonly ILog Log = LogManager.GetCurrentClassLogger();
+
+        #region Constructors
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindowViewModel"/> class.
+        /// </summary>
+        public MainWindowViewModel()
+        {
+
+        }
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Gets the title of the view model.
+        /// </summary>
+        /// <value>The title.</value>
+        public override string Title
+        {
+            get { return "Orc.Analytics.Example"; }
+        }
+        #endregion
+
+        #region Commands
+
+        #endregion
+
+        #region Methods
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override Task Close()
+        {
+            return base.Close();
+        }
+        #endregion
+    }
+}
