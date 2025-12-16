241c4bd349ef2116c349d819d260a2dd89c6dc77
Geert van Horrik <geert@catenalogic.com>
2025-12-16 09:19:05 +0100

    Update sourcelink to 8.0.0

--- src/SolutionAssemblyInfo.cs
+++ src/SolutionAssemblyInfo.cs
@@ -6,8 +6,8 @@
 using System.Reflection;
 
 [assembly: AssemblyCompany("WildGums")]
-[assembly: AssemblyVersion("5.0.0")]
-[assembly: AssemblyFileVersion("5.0.0")]
-[assembly: AssemblyInformationalVersion("5.0.0-alpha.6")]
-[assembly: AssemblyCopyright("Copyright © WildGums 2014 - 2018")]
+[assembly: AssemblyVersion("5.1.0")]
+[assembly: AssemblyFileVersion("5.1.0")]
+[assembly: AssemblyInformationalVersion("5.1.0-alpha.875")]
+[assembly: AssemblyCopyright("Copyright © WildGums 2014 - 2025")]
 
